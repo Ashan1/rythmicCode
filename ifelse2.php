e9d7c771b7219d3a9d7f96cef0326b66f50650ba
NalindaHemanga <nalinda2hemanga@gmail.com>
2015-11-06 06:11:40 +0530

    added lessons

---
+++ ifelse2.php
@@ -0,0 +1,61 @@
+
+            <div class="col-lg-12 textfields" >
+                <div class="col-lg-6 text01" id="editor">
+var instrument="piano";
+
+    
+    
+    if(instrument=="piano"){
+        play("piano","G");
+        play("piano","G");
+        play("piano","G");
+        play("piano","G"); 
+    }
+    else{
+
+        play("flute","G");
+        play("flute","G");
+        play("flute","G");
+        play("flute","G"); 
+
+    }
+
+   
+                
+                
+                </div>
+                <script src="src-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
+                <script>
+                    var editor = ace.edit("editor");
+                    editor.setTheme("ace/theme/monokai");
+                    editor.getSession().setMode("ace/mode/javascript");
+                </script>
+
+                <div class="col-lg-6 text02" id="test">
+                    Change value of the instrument variable to a value of your choice and play the script. Did you notice any Different?
+                    <a onclick="f2()"> Next Step >></a>
+
+
+                </div>
+                <script>
+                    function tryIt(){
+                        var value = editor.getValue();
+                       // document.getElementById("test").innerHTML = 
+                        eval(value);
+                    }
+                </script>
+
+            </div>
+
+            <div class="col-lg-6 pagenumber">
+                <nav>
+                    <ul class="pagination">
+                        <li><a href="#">1</a></li>
+                        <li><a href="#">2</a></li>
+                        <li><a href="#">3</a></li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+        
