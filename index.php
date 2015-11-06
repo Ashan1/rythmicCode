56ece140ff06ea82a1f43daae5eeef25b5e02243
pmartz92 <pmartz92@gmail.com>
2015-11-06 07:27:43 +0530

    index updated

--- index.php
+++ index.php
@@ -110,7 +110,7 @@
     <div class="col-lg-12">
 
                 <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    This is an online teaching and learning module created based on a research paper done by the professors Roger Dannenberg, Frances Dannenberg and Philip Miller of Carnegie Mellon University - Australia. A beginner programmer can use this website to grasp the concepts of programming and try out programming in different languages.
         </p>
 
 
